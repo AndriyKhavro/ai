8240a8c5d725dca28147349544c10c2e8516a001
Andriy Khavro <akh@imtc.com>
2024-02-25 20:52:01 +0200

    Implement MinesweeperAI and Sentence methods

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -105,27 +105,33 @@ class Sentence():
         """
         Returns the set of all cells in self.cells known to be mines.
         """
-        raise NotImplementedError
+        return self.cells.copy() if self.count == len(self.cells) else set()
 
     def known_safes(self):
         """
         Returns the set of all cells in self.cells known to be safe.
         """
-        raise NotImplementedError
+        return self.cells.copy() if self.count == 0 else set()
 
     def mark_mine(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be a mine.
         """
-        raise NotImplementedError
+        if cell in self.cells:
+            self.cells.remove(cell)
+            self.count -= 1
 
     def mark_safe(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be safe.
         """
-        raise NotImplementedError
+        if (cell in self.cells):
+            self.cells.remove(cell)
+
+    def is_completed(self):
+        return len(self.known_mines()) > 0 or len(self.known_safes()) > 0
 
 
 class MinesweeperAI():
@@ -145,9 +151,10 @@ class MinesweeperAI():
         # Keep track of cells known to be safe or mines
         self.mines = set()
         self.safes = set()
+        self.cells = [(i, j) for i in range(height) for j in range(width)]
 
         # List of sentences about the game known to be true
-        self.knowledge = []
+        self.knowledge: list[Sentence] = []
 
     def mark_mine(self, cell):
         """
@@ -167,7 +174,7 @@ class MinesweeperAI():
         for sentence in self.knowledge:
             sentence.mark_safe(cell)
 
-    def add_knowledge(self, cell, count):
+    def add_knowledge(self, cell: tuple[int, int], count: int):
         """
         Called when the Minesweeper board tells us, for a given
         safe cell, how many neighboring cells have mines in them.
@@ -182,7 +189,16 @@ class MinesweeperAI():
             5) add any new sentences to the AI's knowledge base
                if they can be inferred from existing knowledge
         """
-        raise NotImplementedError
+        self.moves_made.add(cell)
+        self.mark_safe(cell)
+        neighbors = list(self.__get_neighbors(cell))
+        unknown_neighbors = [n for n in neighbors if not n in self.mines | self.safes]
+        
+        if len(unknown_neighbors) > 0:
+            known_mines_count = len([n for n in neighbors if n in self.mines])
+            self.knowledge.append(Sentence(unknown_neighbors, count - known_mines_count))
+        
+        self.__refresh()
 
     def make_safe_move(self):
         """
@@ -193,7 +209,10 @@ class MinesweeperAI():
         This function may use the knowledge in self.mines, self.safes
         and self.moves_made, but should not modify any of those values.
         """
-        raise NotImplementedError
+        safe_moves = [cell for cell in self.safes if cell not in self.moves_made]
+        if len(safe_moves) == 0:
+            return None
+        return random.choice(safe_moves)
 
     def make_random_move(self):
         """
@@ -202,4 +221,57 @@ class MinesweeperAI():
             1) have not already been chosen, and
             2) are not known to be mines
         """
-        raise NotImplementedError
+        moves = [cell for cell in self.cells if cell not in self.moves_made | self.mines]
+        if len(moves) == 0:
+            return None
+        return random.choice(moves)
+    
+    def __get_neighbors(self, cell: tuple[int, int]):
+        row, col = cell
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]
+        new_cells = [(row + row_diff, col + col_diff) for row_diff, col_diff in directions]
+        return [(new_row, new_col) for new_row, new_col in new_cells 
+                if 0 <= new_row < self.height and 0 <= new_col < self.width]
+
+    def __refresh(self):
+        while True:
+            completed = [s for s in self.knowledge if s.is_completed()]
+            if len(completed) == 0:
+                self.__infer()
+                completed = [s for s in self.knowledge if s.is_completed()]
+                if len(completed) == 0:
+                    return
+            
+            for sentence in completed:
+                for mine in sentence.known_mines():
+                    self.mark_mine(mine)                
+                for safe in sentence.known_safes():
+                    self.mark_safe(safe)
+            self.knowledge = [s for s in self.knowledge if s not in completed]
+    
+    def __infer(self):
+        supersets = []
+        inferred = []
+        while True:
+            for sentence in self.knowledge:
+                subsets = [s for s in self.knowledge if s != sentence and s.cells.issubset(sentence.cells)]
+                if len(subsets) > 0:
+                    supersets.append(sentence)
+                for subset in subsets:
+                    inferred_cells = sentence.cells - subset.cells
+                    inferred_count = sentence.count - subset.count
+                    inferred_sentence = Sentence(inferred_cells, inferred_count)
+                    inferred.append(inferred_sentence)
+            if len(inferred) == 0:
+                return
+            self.knowledge = [s for s in self.knowledge if s not in supersets]
+            self.knowledge.extend(inferred)
+            inferred.clear()
+            supersets.clear()
+
+
+        
+        
+                
+
+        
